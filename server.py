3bb0dc6debbc132cf088c9982508017871bcd529
Reagan Meek <rmeek@usc.edu>
2022-02-11 13:58:29 -0800

    working server, new frontend

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
 import requests
+import json
 from flask import Flask, jsonify, request
 
-app = Flask('metaspam')
+app = Flask('metaspam', static_folder='./web', static_url_path='/')
 
+spams = []
 @app.route('/', methods=['GET'])
 def homePage():
-    return "yooooooo", 200
+    return app.send_static_file('index.html')
 
+@app.route('/spams', methods=['GET'])
+def getSpams():
+    return jsonify(spams)
+
+@app.route('/spam', methods=['POST'])
+def postSpam():
+    #ip_address = request.remote_addr
+    #print(ip_address)
+    body = json.loads(request.data)
+    #obj = {'ip': ip_address, 'message': body['message']}
+    spams.append(body['message'])
+    return 'added post', 200
 
 app.run(port=8080)
